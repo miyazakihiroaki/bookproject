a3d52d9aaafad7b999f3c00b5057bf82b775a622
MiyazakiHiroaki <h12.miyazakihiroaki@gmail.com>
2022-06-30 18:48:31 +0900

    0630

--- book/views.py
+++ book/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout
 from django.urls import reverse_lazy 
 from django.views.generic import (ListView, DetailView, CreateView, DeleteView, UpdateView,)
 from .models import Book
+from django.views.generic import TemplateView
 
 
 class ListBookView(ListView):
@@ -21,14 +22,16 @@ class CreateBookView(CreateView):
     model = Book
     fields = ('title', 'text', 'category')
     # 操作が完了した後にどこのページに飛ぶのかを決める
-    success_url = reverse_lazy('list-book')
+    # success_url = reverse_lazy('list-book')
+    success_url = reverse_lazy('finish-add')
 
 
 class DeleteBookView(DeleteView):
     template_name = 'book/book_confirmdelete.html'
     model = Book
     # 操作が完了した後にどこのページに飛ぶのかを決める
-    success_url = reverse_lazy('list-book')
+    # success_url = reverse_lazy('list-book')
+    success_url = reverse_lazy('finish-delete')
     
 
 class UpdateBookView(UpdateView):
@@ -36,8 +39,20 @@ class UpdateBookView(UpdateView):
     fields = ('title', 'text', 'category')
     template_name = 'book/book_update.html'
     # 操作が完了した後にどこのページに飛ぶのかを決める
-    success_url = reverse_lazy('list-book')
+    # success_url = reverse_lazy('list-book')
+    success_url = reverse_lazy('finish-update')
 
+
+class FinishDeleteView(TemplateView):
+    template_name = 'book/finish_delete.html'
+    
+
+class FinishAddView(TemplateView):
+    template_name = 'book/finish_add.html'
+
+
+class FinishUpdateView(TemplateView):
+    template_name = 'book/finish_update.html'
 
 def logout_view(request):
     logout(request)
