6bfd21ab261a75a2bf2ff91b2be33b0be486a141
MiyazakiHiroaki <h12.miyazakihiroaki@gmail.com>
2022-07-10 19:42:14 +0900

    modify0710-2

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# これ書いたままデプロイするな！
-SECRET_KEY = ''
+# デプロイするときに注意
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
