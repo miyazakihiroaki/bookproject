3c677d86ca2d7b5142c3965839d3f330b7376685
MiyazakiHiroaki <h12.miyazakihiroaki@gmail.com>
2022-07-21 21:22:29 +0900

    0721-2

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import django-storages
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
